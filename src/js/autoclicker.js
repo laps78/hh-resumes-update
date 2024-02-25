36d2944ab5c8c050ff2044881c97ebb58d4a2d86
Evgeniy Laptev <79728932+laps78@users.noreply.github.com>
2024-02-25 18:45:05 +0300

    MVP готов

--- src/js/autoclicker.js
+++ src/js/autoclicker.js
@@ -3,23 +3,31 @@ console.info(
   "Хочешь разрабатывать с нами новые полезные приложения для пользователей, браузерные расширения, новые фичи? Заходи на сайт проекта https://prolaps.ru! Там можно найти всю требуемую информацию! Присоединяйся к команде!"
 );
 
-//init app
-const localStorage = chrome.storage.local;
+// init app
+const intervalTime = 15 * 60 * 1000;
 
-const intervalTime = 10 * 60 * 1000;
+const getTimeStamp = () => {
+  const now = new Date();
+  const timestamp = now.toLocaleString("ru-RU");
+  return timestamp;
+};
 
+// полезные действия расширения
 function hhRuResumeUpdater() {
   try {
-    console.log("Запускаем кликер по расписанию...");
+    console.info(
+      `[${getTimeStamp()}]: Запускаем HH.RU кликер по расписанию...`
+    );
     const resumeUpdateLinks = document.querySelectorAll(".bloko-link");
     if (resumeUpdateLinks) {
       resumeUpdateLinks.forEach((link) => {
         if (link.textContent === "Поднять в поиске") {
+          const timestamp = getTimeStamp();
           link.click();
-          console.log("Успешно поднял ваше резюме!");
+          console.info(`[${getTimeStamp()}]: Успешно поднял ваше резюме!`);
         } else {
-          console.log(
-            `Не найдены ссылки со словами "Поднять в поиске", пропускаю до следущей проверки через ${
+          console.info(
+            `[${getTimeStamp()}]: Не найдены ссылки со словами "Поднять в поиске", пропускаю до следущей проверки через ${
               intervalTime / 60000
             } минут.`
           );
@@ -36,44 +44,28 @@ function hhRuResumeUpdater() {
         ".bloko-modal-close-button"
       );
       if (fuckenBannerCloseButton) {
-        console.log(`Вижу рекламный баннер автоподнятия от hh.ru.`);
+        console.info(
+          `[${getTimeStamp()}]: Вижу рекламный баннер автоподнятия от hh.ru.`
+        );
         fuckenBannerCloseButton.click();
-        console.log(`Баннер hh.ru закрыт.`);
+        console.info(`[${getTimeStamp()}]: Баннер hh.ru закрыт.`);
       } else {
-        console.log(`Баннер не обнаружен. Игнорирую.`);
+        console.info(`[${getTimeStamp()}]: Баннер не обнаружен. Игнорирую.`);
       }
     } catch (error) {
-      console.error(`Ошибка функции работы с баннером hh.ru: `, error);
+      console.error(
+        `[${getTimeStamp()}]: Ошибка функции работы с баннером hh.ru: `,
+        error
+      );
     }
   }
   setTimeout(closeFuckenBanner, 2500);
 }
 
-const saveDataToStorage = async (data) =>
-  await localStorage.set({ [key]: data });
-const readDataFromStorage = () => {};
-const generateId = () => {};
-const prepareData = () => {
-  try {
-    const collectPrepareData = {};
-    const vars = window.globalVars;
-    return preparedData;
-  } catch (error) {
-    console.error(`Ошибка функции подготовки статистических данных: `, error);
-  }
-};
+window.addEventListener("load", () => {
+  // сразу запускаем работу основных функций
+  hhRuResumeUpdater();
 
-const sendStats = async (url, method = "POST", data) => {
-  try {
-    await fetch(url, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: statData,
-    });
-  } catch (err) {
-    console.error(`Ошибка при отправке обезличенной статистики: `, error);
-  }
-};
-
-setInterval(hhRuResumeUpdater, intervalTime);
+  // планируем дежурство в заданном интервале
+  setInterval(hhRuResumeUpdater, intervalTime);
+});
